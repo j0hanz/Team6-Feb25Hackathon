d00301d33c267b53051b440a47d8cf0e1ff69f12
j0hanz <golfarenlj@gmail.com>
2025-02-15 13:07:28 +0100

    Enhance user profile management by adding additional fields

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,66 @@
+from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.translation import gettext_lazy as _
 
 
 class UserProfile(AbstractUser):
-    pass
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('NB', 'Non-binary'),
+        ('O', 'Other'),
+    ]
+
+    SEXUAL_ORIENTATION_CHOICES = [
+        ('H', 'Heterosexual'),
+        ('G', 'Gay'),
+        ('L', 'Lesbian'),
+        ('B', 'Bisexual'),
+        ('A', 'Asexual'),
+        ('P', 'Pansexual'),
+        ('Q', 'Queer'),
+        ('O', 'Other'),
+    ]
+
+    THEME_CHOICES = [
+        ('light', 'Light'),
+        ('dark', 'Dark'),
+    ]
+
+    FONT_FAMILY_CHOICES = [
+        ('default', 'Default'),
+        ('serif', 'Serif'),
+        ('sans-serif', 'Sans-serif'),
+        ('dyslexie', 'Dyslexie'),
+    ]
+
+    profile_image = CloudinaryField('Profile Image', null=True, blank=True)
+    gender = models.CharField(max_length=2, choices=GENDER_CHOICES, blank=True)
+    sexual_orientation = models.CharField(
+        max_length=2, choices=SEXUAL_ORIENTATION_CHOICES, blank=True
+    )
+    age = models.PositiveIntegerField(null=True, blank=True)
+    about = models.TextField(blank=True)
+    interests = models.TextField(blank=True)
+    theme = models.CharField(
+        max_length=5, choices=THEME_CHOICES, default='light'
+    )
+    font_family = models.CharField(
+        max_length=10, choices=FONT_FAMILY_CHOICES, default='default'
+    )
+
+    class Meta:
+        verbose_name = _('User Profile')
+        verbose_name_plural = _('User Profiles')
+
+    def __str__(self) -> str:
+        return self.username
+
+
+@receiver(post_save, sender=UserProfile)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        pass
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from .forms import UserProfileChangeForm
+
 
 def profile_page(request):
-    return render(request, 'profiles/profile.html')
+    if request.method == 'POST':
+        form = UserProfileChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserProfileChangeForm(instance=request.user)
+    return render(request, 'profiles/profile.html', {'form': form})

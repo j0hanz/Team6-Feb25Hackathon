3ddd55455a634584e1f3e1a2bd12abf2535b4e1e
j0hanz <golfarenlj@gmail.com>
2025-02-16 12:39:07 +0100

    Add match profiles feature with filtering options

--- project_root/urls.py
+++ project_root/urls.py
@@ -7,6 +7,7 @@ from home.views import hello_world
 
 urlpatterns = [
     path('', hello_world, name='home'),
+    path('match/', include('home.urls')),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
     path('about/', include('about.urls')),

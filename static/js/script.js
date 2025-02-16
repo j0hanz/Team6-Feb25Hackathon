c372f3b8e11cf30852babe49cd2e73ffa895db9c
j0hanz <golfarenlj@gmail.com>
2025-02-16 21:25:53 +0100

    Add fade-in effect on page load and improve theme switcher functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,32 +1,35 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Get the theme switcher element
-    const themeSwitcher = document.getElementById('theme-switcher');
-    if (themeSwitcher) {
-        themeSwitcher.addEventListener('change', function () {
-            document.body.className = this.value;
-            updateNavLinkColors();
-        });
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  // Add the loaded class to the body to show the content
+  document.body.classList.add("loaded");
 
-    // Get the font family selector element
-    const fontFamilySelector = document.getElementById('font-family-selector');
-    if (fontFamilySelector) {
-        fontFamilySelector.addEventListener('change', function () {
-            document.body.style.fontFamily = this.value;
-        });
-    }
+  // Get the theme switcher element
+  const themeSwitcher = document.getElementById("theme-switcher");
+  if (themeSwitcher) {
+    themeSwitcher.addEventListener("change", function () {
+      document.body.className = this.value;
+      updateNavLinkColors();
+    });
+  }
 
-    function updateNavLinkColors() {
-        const navLinks = document.querySelectorAll('.navLink');
-        navLinks.forEach(link => {
-            if (document.body.classList.contains('dark')) {
-                link.style.color = '#ffffff';
-            } else {
-                link.style.color = '#000000';
-            }
-        });
-    }
+  // Get the font family selector element
+  const fontFamilySelector = document.getElementById("font-family-selector");
+  if (fontFamilySelector) {
+    fontFamilySelector.addEventListener("change", function () {
+      document.body.style.fontFamily = this.value;
+    });
+  }
 
-    // Initial call to set the correct colors
-    updateNavLinkColors();
+  function updateNavLinkColors() {
+    const navLinks = document.querySelectorAll(".navLink");
+    navLinks.forEach((link) => {
+      if (document.body.classList.contains("dark")) {
+        link.style.color = "#ffffff";
+      } else {
+        link.style.color = "#000000";
+      }
+    });
+  }
+
+  // Initial call to set the correct colors
+  updateNavLinkColors();
 });

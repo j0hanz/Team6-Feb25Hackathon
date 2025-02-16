3807aa8064fe67e0bb89dfe5f56a82a2d26f7214
j0hanz <golfarenlj@gmail.com>
2025-02-16 19:44:07 +0100

    Add auto match profiles feature and enhance matching criteria

--- home/views.py
+++ home/views.py
@@ -10,16 +10,36 @@ def hello_world(request):
 def match_profiles(request):
     gender = request.GET.get('gender')
     sexual_orientation = request.GET.get('sexual_orientation')
+    age_range = request.GET.get('age_range')
+    interests = request.GET.get('interests')
     profiles = UserProfile.objects.none()
 
-    if gender or sexual_orientation:
+    if gender or sexual_orientation or age_range or interests:
         profiles = UserProfile.objects.all()
         if gender:
             profiles = profiles.filter(gender=gender)
         if sexual_orientation:
             profiles = profiles.filter(sexual_orientation=sexual_orientation)
+        if age_range:
+            profiles = profiles.filter(age_range=age_range)
+        if interests:
+            profiles = profiles.filter(interests=interests)
 
     return render(request, 'home/match.html', {'profiles': profiles})
+
+
+def auto_match_profiles(request):
+    user = request.user
+    profiles = UserProfile.objects.none()
+
+    if user.is_authenticated:
+        profiles = UserProfile.objects.exclude(id=user.id)
+        if user.interests:
+            profiles = profiles.filter(gender=user.interests)
+        if user.age_range:
+            profiles = profiles.filter(age_range=user.age_range)
+
+    return render(request, 'home/auto_match.html', {'profiles': profiles})
 
 
 def user_profile(request, user_id):
